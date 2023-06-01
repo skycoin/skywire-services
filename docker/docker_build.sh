8c9fc94a2ae3871e4ff0d298f434a80b8d4480fe
Mohammed <mrpalide@gmail.com>
2023-06-01 13:42:00 +0330

    use github_token instead ssh key 4

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -45,8 +45,8 @@ if [[ "$image_tag" == "e2e" ]]; then
   fi
 
   # TODO(ersonp): instead of cloning the git branch we should directly use the docker image od SD from dockerhub like we doing for dmsg 
-  if [[ "$GITHUB_TOKEN" != "" ]]; then
-    git clone https://"$GITHUB_TOKEN"@github.com/skycoin/skywire-ut --depth 1 --branch "$git_branch" ./tmp/skywire-ut
+  if [[ "$GIT_TOKEN" != "" ]]; then
+    git clone https://"$GIT_TOKEN"@github.com/skycoin/skywire-ut --depth 1 --branch "$git_branch" ./tmp/skywire-ut
   else
     git clone git@github.com:skycoin/skywire-ut --depth 1 --branch "$git_branch" ./tmp/skywire-ut
   fi
