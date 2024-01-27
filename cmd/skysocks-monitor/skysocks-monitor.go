07b0f3e2656df929d5400322b19d1a6fa0700168
MohammadReza Palide <mrpalide@gmail.com>
2024-01-27 09:37:43 +0000

    implement skysocks-monitor

---
+++ cmd/skysocks-monitor/skysocks-monitor.go
@@ -0,0 +1,8 @@
+// Package main cmd/skysocks-monitor/skysocks-monitor.go
+package main
+
+import "github.com/skycoin/skywire-services/cmd/skysocks-monitor/commands"
+
+func main() {
+	commands.Execute()
+}
