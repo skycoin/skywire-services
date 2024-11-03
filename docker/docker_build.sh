2afcb1a90c6aa34986d607a8a360cee3890fea30
Mohammed <mrpalide@gmail.com>
2024-11-03 08:12:59 +0000

    upgrade go version from 1.21 to 1.23

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -15,7 +15,7 @@ platform="--platform=linux/amd64"
 registry="$REGISTRY"
 
 # shellcheck disable=SC2153
-base_image=golang:1.21-alpine
+base_image=golang:1.23-alpine
 
 if [[ "$#" != 2 ]]; then
   echo "docker_build.sh <IMAGE_TAG> <GO_BUILDOPTS>"
