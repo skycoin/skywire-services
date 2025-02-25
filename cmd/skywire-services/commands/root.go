6d5e6aca990e0c0343903c1fbb81878a33903c12
Mohammed <mrpalide@gmail.com>
2025-02-25 08:01:58 +0000

    move services to skywire

--- cmd/skywire-services/commands/root.go
+++ cmd/skywire-services/commands/root.go
@@ -11,37 +11,13 @@ import (
 	"github.com/skycoin/skywire/pkg/skywire-utilities/pkg/buildinfo"
 	"github.com/spf13/cobra"
 
-	ar "github.com/skycoin/skywire-services/cmd/address-resolver/commands"
-	confbs "github.com/skycoin/skywire-services/cmd/config-bootstrapper/commands"
-	kg "github.com/skycoin/skywire-services/cmd/keys-gen/commands"
 	nv "github.com/skycoin/skywire-services/cmd/node-visualizer/commands"
-	rf "github.com/skycoin/skywire-services/cmd/route-finder/commands"
-	se "github.com/skycoin/skywire-services/cmd/sw-env/commands"
-	tpd "github.com/skycoin/skywire-services/cmd/transport-discovery/commands"
-	tps "github.com/skycoin/skywire-services/cmd/transport-setup/commands"
-	ut "github.com/skycoin/skywire-services/cmd/uptime-tracker/commands"
 )
 
 func init() {
 	RootCmd.AddCommand(
-		tpd.RootCmd,
-		tps.RootCmd,
-		ar.RootCmd,
-		rf.RootCmd,
-		confbs.RootCmd,
-		kg.RootCmd,
-		se.RootCmd,
-		ut.RootCmd,
 		nv.RootCmd,
 	)
-	tpd.RootCmd.Use = "tpd"
-	tps.RootCmd.Use = "tps"
-	ar.RootCmd.Use = "ar"
-	rf.RootCmd.Use = "rf"
-	confbs.RootCmd.Use = "confbs"
-	kg.RootCmd.Use = "kg"
-	se.RootCmd.Use = "se"
-	ut.RootCmd.Use = "ut"
 	nv.RootCmd.Use = "nv"
 }
 
